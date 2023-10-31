b9e2d1536b7c39b0d665500506458d87eb805382
sunnyvicks <143432796+sunnyvicks@users.noreply.github.com>
2023-10-31 08:40:35 -0400

    login access_token

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -10,14 +10,20 @@ export const options: NextAuthOptions = {
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      //   profile(profile) {
-      //     return {
-      //       id: profile.id,
-      //       name: profile.display_name,
-      //       email: profile.email,
-      //       image: profile.images?.[0]?.url,
-      //     };
-      //   },
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,13 @@ import styles from "./page.module.css";
 import { options } from "./api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
 import Scene from "./Scene";
+import { getToken } from "next-auth/jwt";
+import { useEffect } from "react";
+const secret = process.env.NEXTAUTH_SECRET;
+
 export default async function Home() {
   const session = await getServerSession(options);
+  console.log("session", session);
   return (
     <>
       {session ? <div>Logged in! {session?.user?.name}</div> : <div>Login</div>}
