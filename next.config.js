ed96c518bcf3f9add440be61931068351338cacd
sunnyvicks <143432796+sunnyvicks@users.noreply.github.com>
2023-12-12 17:46:43 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -6,20 +6,19 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
     prependData: `@import "@/styles/variables.scss";`,
-  }
+  },
   /**
    * Enable static exports for the App Router.
    *
    * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-   */,
-  output: "export",
+   */ output: "export",
 
   /**
    * Set base path. This is usually the slug of your repository.
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/nextjs-github-pages",
+  basePath: "/music_3d_scene",
 
   /**
    * Disable server-based image optimization. Next.js does not support
